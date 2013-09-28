d2eea06856c57b7049a78cf1f2fe59410d875942
andylinteau <andy.linteau@buystand.com>
2013-09-29 02:43:14 +0800

    add header and footer style home page

--- Gemfile
+++ Gemfile
@@ -1,8 +1,8 @@
-source 'https://rubygems.org'
+source 'http://rubygems.org'
 ruby '2.0.0'
 
 gem 'rails', '4.0.0'
-gem 'bootstrap-sass', '~> 2.3.2.1'
+gem 'bootstrap-sass', '2.3.2.0'
 # Use sqlite3 as the database for Active Record
 
 
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -25,4 +25,18 @@ describe "StaticPages" do
     it { should have_content('Contact Us')}
   end
   
+  it "should have the right links on the layout" do
+    visit root_path
+    click_link "Home"
+    expect(page).to have_title("Home")
+    click_link "Find-WIFI"
+    expect(page).to have_title("Find-WIFI")
+    click_link "About"
+    expect(page).to have_title("About Us")
+    click_link "About Us"
+    expect(page).to have_title("About Us")
+    click_link "Contact"
+    expect(page).to have_title("Contact")
+  end
+  
 end
